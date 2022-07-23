fd800fb2ca6e25b83f2802c9520e271dfd4e6668
bdaubry <baubry@aais.com>
2022-07-23 16:15:55 -0500

    meeting details page

---
+++ src/pages/meet/MeetDeets.tsx
@@ -0,0 +1,84 @@
+import { LoaderFn, MakeGenerics, useMatch } from "@tanstack/react-location";
+import { Box, Button, Link, Grid, Text, Stack, Heading, Avatar, Flex, Spacer, Wrap, WrapItem, ButtonGroup, GridItem } from '@chakra-ui/react';
+import { CSSProperties } from "react";
+
+interface Props {
+  roomId: string;
+  username: string;
+  updateRoomId?: React.Dispatch<React.SetStateAction<string | undefined>>;
+  updateUsername: React.Dispatch<React.SetStateAction<string>>;
+  updateTokens: React.Dispatch<
+    React.SetStateAction<{ clientToken: string; refreshToken: string }>
+  >;
+  clientToken: string;
+  refreshToken: string;
+}
+
+const headingStyle: CSSProperties = {
+  color: "#A0AEC0",
+  fontWeight: "700",
+}
+
+const pad = '5';
+
+export const MeetDeets = () => {
+  return (
+    <Box borderWidth='1px' borderRadius='lg' overflow='hidden' m='8'>
+      <Grid
+        h='200px'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={4}
+      >
+        <GridItem rowSpan={2} colSpan={1}>
+          <Flex flexDirection={'column'} justifyContent={'space-between'}>
+            <Box p={pad}>
+              <Wrap>
+                <WrapItem>
+                  <Stack>
+                    <Heading as='h4' size='md'>Upcoming Meeting</Heading>
+                    <Link>12345</Link>
+                  </Stack>
+                </WrapItem>
+              </Wrap>
+            </Box>
+            <Spacer />
+            <Box p={pad}>
+              <ButtonGroup gap='2'>
+                <Button colorScheme='green' size='lg' variant='solid'>
+                  Join
+                </Button>
+                <Button colorScheme='red' size='lg' variant='solid'>
+                  Reject
+                </Button>
+              </ButtonGroup>
+            </Box>
+          </Flex>
+        </GridItem>
+        <GridItem rowSpan={2} colSpan={1} p={pad}>
+          <Heading as='h6' size='s' style={headingStyle}>
+            Mentor
+          </Heading>
+          <Flex dir="row" flexWrap={'nowrap'}>
+            <Avatar size='sm' />
+            <Text>Mentor Name</Text>
+          </Flex>
+        </GridItem>
+        <GridItem rowSpan={2} colSpan={1} p={pad}>
+          <Heading as='h6' size='s' style={headingStyle}>
+            Date
+          </Heading>
+          <Text>August 1st, 2022</Text>
+        </GridItem>
+        <GridItem rowSpan={2} colSpan={1} p={pad}>
+          <Heading as='h6' size='s' style={headingStyle}>
+            Time
+          </Heading>
+          <Text>5:30 PM</Text>
+        </GridItem>
+      </Grid>
+    </Box>
+  );
+};
+
+export default MeetDeets;
