fad7e55e4373d4f5beef42264b24ecf91a374f52
Christian Barlow <c.barlow108@gmail.com>
2022-07-23 14:21:53 -0500

    add file based routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './pages'
 import './index.css'
+
+import { Routes } from 'generouted'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>
 )
