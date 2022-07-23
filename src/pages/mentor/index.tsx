34812d966aee570e704552eba9753547864d831e
Christian Barlow <c.barlow108@gmail.com>
2022-07-23 14:40:39 -0500

    add browse, mentor pafe & mentor home page

---
+++ src/pages/mentor/index.tsx
@@ -0,0 +1,11 @@
+import { LoaderFn, MakeGenerics, useMatch } from "@tanstack/react-location";
+
+export const MentorHome = () => {
+  return (
+    <div>
+      <h1>Mentor Home</h1>
+    </div>
+  );
+};
+
+export default MentorHome;
