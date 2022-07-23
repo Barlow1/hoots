dbf8c960d264fbde9a95a6ef0edb9a619fb7b216
Jessie Murphey <85692381+cautiousbrick@users.noreply.github.com>
2022-07-23 17:46:44 -0500

    Update goals.tsx
    adding css changes

--- src/pages/goals.tsx
+++ src/pages/goals.tsx
@@ -1,6 +1,13 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Progress,
+  useStyles,
+} from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import * as React from "react";
-
 export interface UserGoal {
   goal: string;
   dueDate: string;
@@ -11,27 +18,27 @@ const GoalsPage = () => {
   const [userGoals, setUserGoals] = React.useState<UserGoal[]>([]);
   React.useEffect(() => {
     setUserGoals([
-        {
-            goal: 'Learn React and Redux',
-            dueDate: 'December 1st, 2023',
-            progress: 75
-        },
-        {
-            goal: 'Get an internship',
-            dueDate: 'June 10th, 2023',
-            progress: 100
-        },
-        {
-            goal: 'Create a portfolio page',
-            dueDate: 'January 1st, 2023',
-            progress: 100
-        },
-        {
-            goal: 'Learn Javascript',
-            dueDate: 'August 21st, 2022',
-            progress: 100
-        },
-      ]);
+      {
+        goal: "Learn React and Redux",
+        dueDate: "December 1st, 2023",
+        progress: 75,
+      },
+      {
+        goal: "Get an internship",
+        dueDate: "June 10th, 2023",
+        progress: 100,
+      },
+      {
+        goal: "Create a portfolio page",
+        dueDate: "January 1st, 2023",
+        progress: 100,
+      },
+      {
+        goal: "Learn Javascript",
+        dueDate: "August 21st, 2022",
+        progress: 100,
+      },
+    ]);
   });
 
   return (
@@ -41,21 +48,48 @@ const GoalsPage = () => {
   );
 };
 
+export const gridItemStyle: React.CSSProperties = {
+  padding: "1rem",
+  borderTop: "2px solid #E2E8F0",
+};
 export interface IGoalsContainerProps {
   userGoals: UserGoal[];
 }
 export const GoalsContainer = ({ userGoals }: IGoalsContainerProps) => {
   return (
-    <Box>
-      <Grid>
+    <Box style={{ width: "100%", height: "100%" }}>
+      <Box style={{ width: "100%", textAlign: "right" }}>
+        <Button backgroundColor={"brand.500"} style={{ color: "white", margin: '1rem' }}>
+          Add Goal <AddIcon style={{}} />
+        </Button>
+      </Box>
+      <Grid
+        templateColumns="repeat(3, 1fr)"
+        style={{
+          border: "2px solid #E2E8F0",
+          borderRadius: "10px",
+          padding: "1rem",
+        }}
+      >
+        <GridItem style={{ padding: "1rem", fontWeight: "bold" }}>
+          Goal
+        </GridItem>
+        <GridItem style={{ padding: "1rem", fontWeight: "bold" }}>Due</GridItem>
+        <GridItem
+          style={{ padding: "1rem", textAlign: "right", fontWeight: "bold" }}
+        >
+          Progress
+        </GridItem>
         {userGoals.map((item, index) => {
           return (
-            <GoalsItem
-              key={`goal-${index}`}
-              goal={item.goal}
-              dueDate={item.dueDate}
-              progress={item.progress}
-            />
+            <>
+              <GoalsItem
+                key={`goal-${index}`}
+                goal={item.goal}
+                dueDate={item.dueDate}
+                progress={item.progress}
+              />
+            </>
           );
         })}
       </Grid>
@@ -66,9 +100,22 @@ export const GoalsContainer = ({ userGoals }: IGoalsContainerProps) => {
 export const GoalsItem = ({ goal, dueDate, progress }: UserGoal) => {
   return (
     <>
-      <GridItem>{goal}</GridItem>
-      <GridItem>{dueDate}</GridItem>
-      <GridItem>{progress}</GridItem>
+      <GridItem style={gridItemStyle}>{goal}</GridItem>
+      <GridItem style={gridItemStyle}>{dueDate}</GridItem>
+      <GridItem style={{ ...gridItemStyle, textAlign: "right" }}>
+        {progress < 100 && (
+          <>
+            <Progress
+              colorScheme="green"
+              size="sm"
+              style={{ borderRadius: "500px" }}
+              value={progress}
+            />
+            {progress}%
+          </>
+        )}
+        {progress === 100 && "Complete"}
+      </GridItem>
     </>
   );
 };
