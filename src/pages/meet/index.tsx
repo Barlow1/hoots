c61a34e5cb4bed50831bd0bc5b167847c95d1369
bdaubry <baubry@aais.com>
2022-07-23 14:53:33 -0500

    layout for meet

---
+++ src/pages/meet/index.tsx
@@ -0,0 +1,11 @@
+import { LoaderFn, MakeGenerics, useMatch } from "@tanstack/react-location";
+
+export const MeetHome = () => {
+  return (
+    <div>
+      <h1>Meet Home</h1>
+    </div>
+  );
+};
+
+export default MeetHome;
---
+++ src/pages/meet/room/index.tsx
@@ -0,0 +1,11 @@
+import { LoaderFn, MakeGenerics, useMatch } from "@tanstack/react-location";
+
+export const Room = () => {
+  return (
+    <div>
+      <h1>room lol</h1>
+    </div>
+  );
+};
+
+export default Room;
