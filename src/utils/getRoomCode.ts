8f916a51a8f92d125b745c9ac24d31f263420882
Brian Aubry <bdaubry@gmail.com>
2022-07-23 21:00:28 -0500

    fix api

--- src/utils/getRoomCode.ts
+++ src/utils/getRoomCode.ts
@@ -12,17 +12,28 @@ async function createRoom(name?: string) {
           Authorization: 'Bearer KEY0181B6DECC71CD9E97B9D3190B23495D_uynCmXv11YVl5BBr6gENX5'
         }
       },
-      data: {body: {enable_recording:false,max_participants:2}}
+      data: {enable_recording:false,max_participants:2}
   };
   
-  try {
-    const response = await request('post', options.url, undefined, options.data, options.options);
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer KEY0181B6DECC71CD9E97B9D3190B23495D_uynCmXv11YVl5BBr6gENX5'
+    },
+    body: JSON.stringify({ "enable_recording":false,"max_participants":2,"unique_name": name || undefined })
+  };
 
-    const result = (JSON.parse(response.data));
+  try {
+    const response = await fetch('https://api.telnyx.com/v2/rooms', requestOptions).then(response => response.json());
+    //const response = await request('post', options.url, undefined, options.data, options.options);
+
+    const result = response.body;
 
     console.log(result.data.id);
 
-    return result.data.id;
+    return result;
   } catch (error) {
     if (error instanceof Error) {
       console.log('error message: ', error.message);
