942936bbe357b41d987e1a1259b105147b9a9ed9
Christian Barlow <c.barlow108@gmail.com>
2022-07-24 09:44:17 -0500

    fix dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { User } from "@prisma/client";
-import { LoaderFn, MakeGenerics, useMatch } from "@tanstack/react-location";
+import { Profile } from "@prisma/client";
+import { LoaderFn, MakeGenerics, useLocation, useMatch } from "@tanstack/react-location";
 import { useEffect, useState } from "react";
 import { UserContextProvider } from "../components/UserContext";
 import MainContainer from "../container";
@@ -22,16 +22,25 @@ const loader = async () => {
 };
 
 const App = ({ children }: AppProps) => {
-  const [user, setUser] = useState<User | undefined>();
+  const [user, setUser] = useState<Profile | undefined>();
   const fetchUser = async () => {
     const { user } = await loader();
     setUser(user);
   };
+  const location = useLocation();
+ 
   useEffect(() => {
     if (!user) {
       fetchUser();
     }
   }, []);
+
+  console.log(location.current.pathname);
+  
+  if (location.current.pathname === '/') {
+    return <UserContextProvider user={user}>{children}</UserContextProvider>
+  }
+
   return <UserContextProvider user={user}><MainContainer>{children}</MainContainer></UserContextProvider>;
 };
 
