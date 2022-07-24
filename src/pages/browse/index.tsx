67b94b087835545f33fdff10eb54c447402b6a53
Christian Barlow <c.barlow108@gmail.com>
2022-07-24 02:10:34 -0500

    add tags

--- src/pages/browse/index.tsx
+++ src/pages/browse/index.tsx
@@ -1,4 +1,14 @@
-import { Avatar, Box, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Tag,
+  Text,
+} from "@chakra-ui/react";
 import { Mentor } from "@prisma/client";
 import {
   Link,
@@ -31,34 +41,44 @@ const Browse = () => {
       <Heading as="h1" size="xl">
         Browse
       </Heading>
-      {data.mentors?.map((mentor) => {
-        return (
-          <Link to={`${routes.browse}/${mentor.id}`}>
-            <Box
-              maxW="sm"
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              key={mentor.id}
-              p="5"
-              _hover={{ backgroundColor: "gray.100"}}
-            >
-              <Flex justifyContent={"center"}>
-                <Avatar size="md" src={mentor.img} />
-              </Flex>
+      <Grid templateColumns="repeat(3, 1fr)" gap={5} pt="5">
+        {data.mentors?.map((mentor) => {
+          return (
+            <GridItem>
+              <Link to={`${routes.browse}/${mentor.id}`}>
+                <Box
+                  maxW="lg"
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  overflow="hidden"
+                  key={mentor.id}
+                  p="5"
+                  h="100%"
+                  _hover={{ backgroundColor: "gray.100" }}
+                >
+                  <Flex justifyContent={"center"}>
+                    <Avatar size="md" src={mentor.img} />
+                  </Flex>
 
-              <Heading as="h2" size="lg" noOfLines={1}>
-                {mentor.name}
-              </Heading>
-              <Text>💼 {mentor.occupation}</Text>
-              <Text>🏢 {mentor.company}</Text>
-              <Text>🕒 {mentor.experience} years</Text>
-              <Text>💲 {mentor.cost}</Text>
-              <Text noOfLines={3}>{mentor.bio}</Text>
-            </Box>
-          </Link>
-        );
-      })}
+                  <Heading as="h2" size="lg" noOfLines={1}>
+                    {mentor.name}
+                  </Heading>
+                  <Text>💼 {mentor.occupation}</Text>
+                  <Text>🏢 {mentor.company}</Text>
+                  <Text>🕒 {mentor.experience} years</Text>
+                  <Text>💲 {mentor.cost}</Text>
+                  <HStack spacing={2}>
+                    {mentor.tags.map((tag) => {
+                      return <Tag key={tag} background="brand.500" color="white">{tag}</Tag>;
+                    })}
+                  </HStack>
+                  <Text noOfLines={3}>{mentor.bio}</Text>
+                </Box>
+              </Link>
+            </GridItem>
+          );
+        })}
+      </Grid>
     </div>
   );
 };
