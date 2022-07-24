32136f1cac9530aa74285292cddbb4e5447a3440
Alejandro Melgar <alejandromelgar@Alejandros-MBP.lan>
2022-07-23 19:21:53 -0500

    installing formik for forms and setting up first couple fields

--- src/pages/new-user-form.tsx
+++ src/pages/new-user-form.tsx
@@ -4,8 +4,12 @@ import {
   //   FormErrorMessage,
   Input,
   FormHelperText,
+  Box,
+  Text,
+  Button,
 } from "@chakra-ui/react";
-
+import { Field, Form, Formik } from "formik";
+// import { colors } from '../main'
 /*
 About Me:
 - Journey (Area of Interest)
@@ -19,12 +23,43 @@ Desired Mentor:
 */
 
 const NewUserForm = () => {
+  // console.log(brand.200)
   return (
-    <FormControl>
-      <FormLabel>Email address</FormLabel>
-      <Input type="email" />
-      <FormHelperText>We'll never share your email.</FormHelperText>
-    </FormControl>
+    <Box   margin='30%'
+    width='50%' boxShadow='2xl' padding='3' maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>  
+      <Text style={{fontWeight: 'bold'}}>About Me</Text>
+    <Formik
+      initialValues={{ firstName: '', lastName: '' }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          actions.setSubmitting(false)
+        }, 1000)
+      }}
+    >
+      {(props) => (
+        <Form style={{padding: 5}}>
+         <Field name='firstName'>
+            {({ field }) => (
+                <FormControl>
+                  <FormLabel>First name</FormLabel>
+                  <Input {...field} placeholder='First Name' />
+                </FormControl>
+            )}
+         </Field>
+         <Field name='lastName'>
+            {({ field }) => (
+                <FormControl>
+                  <FormLabel>Last name</FormLabel>
+                  <Input {...field} placeholder='Last Name' />
+                </FormControl>
+            )}
+         </Field>
+        <Button marginTop='5px' background='brand.200' isLoading={props.isSubmitting} type='submit'>Button</Button>
+        </Form>
+        )}
+     </Formik>
+    </Box>
   );
 };
 
