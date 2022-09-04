f23facd24ff230b3276ccf4af4d0af01cb96933d
Christian Barlow <c.barlow108@gmail.com>
2022-09-04 11:54:03 -0500

    Use real date picker for milestones & goals

---
+++ app/utils/dates.ts
@@ -0,0 +1,12 @@
+export const formatDateDisplay = (
+  date: string | number | Date,
+  locale: Intl.LocalesArgument = "en-US",
+  options: Intl.DateTimeFormatOptions | undefined = {}
+) =>
+  new Date(date).toLocaleDateString(locale, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: "utc",
+    ...options,
+  });
