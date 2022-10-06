921fe40ac1a4ddf157c39ed99c908ea1e792e986
Christian Barlow <c.barlow108@gmail.com>
2022-10-06 02:25:22 -0500

    add send email again button

--- app/routes/start/check-email.tsx
+++ app/routes/start/check-email.tsx
@@ -3,23 +3,13 @@ import {
   Button,
   Checkbox,
   Flex,
-  FormControl,
-  FormLabel,
   Heading,
   Link,
   Stack,
   Text,
   Image,
   useColorModeValue,
-  Grid,
-  GridItem,
-  useCheckboxGroup,
 } from "@chakra-ui/react";
-import {
-  faMagnifyingGlass,
-  faPersonCirclePlus,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Form, useActionData, useTransition } from "@remix-run/react";
 import {
   ActionFunction,
@@ -29,6 +19,8 @@ import {
 import { userInfo } from "os";
 import LargeCheckBox from "~/components/LargeCheckBox";
 import { routes } from "~/routes";
+import { createVerificationLink } from "~/utils/email-verification.server";
+import { sendEmail } from "~/utils/email.server";
 import { getUser, requireUser } from "~/utils/user.session";
 import Logo from "../../assets/Logo.svg";
 
@@ -40,9 +32,29 @@ export const loader: LoaderFunction = async ({ request }) => {
   return null;
 };
 
-export const action: ActionFunction = async ({ request }) => {};
-
+export const action: ActionFunction = async ({ request }) => {
+  const baseUrl = new URL(request.url).origin;
+  const user = await getUser(request);
+  const verificationLink = createVerificationLink({
+    email: user.email,
+    domainUrl: baseUrl,
+  });
+  sendEmail({
+    toName: `${user.firstName} ${user.lastName}`,
+    fromName: "Hoots",
+    email: user.email,
+    subject: "Email Verification",
+    variables: {
+      firstName: user.firstName,
+      verificationLink,
+    },
+    template: "email-verification",
+  });
+  return true;
+};
 const CheckEmail = () => {
+  const transition = useTransition();
+  const newEmailSent = useActionData();
   return (
     <Flex
       minH={"100vh"}
@@ -57,6 +69,28 @@ const CheckEmail = () => {
           <Text fontSize={"lg"} color={"gray.600"}>
             Check your email for a verification link.
           </Text>
+          <Form method="post">
+            {transition.state === "idle" && !newEmailSent && (
+              <>
+                <Text fontSize={"sm"} color={"gray.600"}>
+                  Don't see it?{" "}
+                  <Link as={"button"} type="submit" color="brand.900">
+                    Send it again
+                  </Link>
+                </Text>
+              </>
+            )}
+            {transition.state === "idle" && newEmailSent && (
+              <Text fontSize={"sm"} color={"gray.600"}>
+                Sent
+              </Text>
+            )}
+            {transition.state !== "idle" && (
+              <Text fontSize={"sm"} color={"gray.600"}>
+                Sending...
+              </Text>
+            )}
+          </Form>
         </Stack>
       </Stack>
     </Flex>
