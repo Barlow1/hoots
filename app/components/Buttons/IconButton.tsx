400284fb2ae488cadef73a114dce050ac74f6cf4
Christian Barlow <c.barlow108@gmail.com>
2022-12-27 12:48:30 -0600

    finish converting start page

--- app/components/Buttons/IconButton.tsx
+++ app/components/Buttons/IconButton.tsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
+import LoadingSpinner from "../LoadingSpinner";
 
 const buttonStyles = {
   primary: "bg-brand-700 text-white hover:bg-brand-600",
   secondary:
-    "bg-brand-100 dark:bg-zinc-700 text-brand-700 dark:text-white hover:bg-brand-200 dark:hover:bg-zinc-600 ",
-  danger: "bg-red-600 dark:bg-red-700 text-white hover:bg-red-500 dark:hover:bg-red-600"
+    "bg-brand-100 dark:bg-zinc-400 text-brand-700 hover:bg-brand-200 dark:hover:bg-zinc-300 ",
+  danger:
+    "bg-red-600 dark:bg-red-700 text-white hover:bg-red-500 dark:hover:bg-red-600",
 };
 export default function Button({
   leftIcon,
@@ -12,24 +14,28 @@ export default function Button({
   children,
   className,
   variant = "secondary",
+  loading = false,
   ...rest
 }: React.PropsWithChildren<
   {
     leftIcon?: JSX.Element;
     rightIcon?: JSX.Element;
     variant?: "primary" | "secondary" | "danger";
-  } & React.HTMLAttributes<HTMLButtonElement> &  React.ButtonHTMLAttributes<HTMLButtonElement>
+    loading?: boolean;
+  } & React.HTMLAttributes<HTMLButtonElement> &
+    React.ButtonHTMLAttributes<HTMLButtonElement>
 >) {
   return (
     <button
       type="button"
       className={clsx(
-        "m-4 inline-flex items-center justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:ring-offset-zinc-900",
+        "m-4 inline-flex items-center justify-center rounded-md disabled:opacity-30 disabled:pointer-events-none border border-transparent px-4 py-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:ring-offset-zinc-900",
         buttonStyles[variant],
         className
       )}
       {...rest}
     >
+      {loading && <LoadingSpinner />}
       {leftIcon}
       {children}
       {rightIcon}
--- app/routes/start/index.tsx
+++ app/routes/start/index.tsx
@@ -1,38 +1,33 @@
 import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Link,
-  Stack,
-  Text,
-  Image,
-  useColorModeValue,
-  Grid,
-  GridItem,
-  useCheckboxGroup,
-} from '@chakra-ui/react';
-import {
   faMagnifyingGlass,
   faPersonCirclePlus,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Form, useActionData, useTransition } from '@remix-run/react';
-import type {
-  ActionFunction,
-  LoaderFunction} from '@remix-run/server-runtime';
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Form, Link, useActionData, useTransition } from "@remix-run/react";
+import type { ActionFunction, LoaderFunction } from "@remix-run/server-runtime";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import {
-  redirect,
-} from '@remix-run/server-runtime';
-import LargeCheckBox from '~/components/LargeCheckBox';
-import { routes } from '~/routes';
-import { requireUser } from '~/utils/user.session.server';
-import Logo from '../../assets/Logo.svg';
+import { redirect } from "@remix-run/server-runtime";
+import React from "react";
+import Button from "~/components/Buttons/IconButton";
+import LargeCheckBox from "~/components/LargeCheckBox";
+import { H4, Paragraph } from "~/components/Typography";
+import { routes } from "~/routes";
+import isObject from "~/utils/isObject";
+import { requireUser } from "~/utils/user.session.server";
+import Logo from "../../assets/Logo.svg";
 
-const SEEKING_MENTOR = 'SeekingMentor';
-const CREATE_MENTOR_PROFILE = 'CreateMentorProfile';
-const USER_START_OPTIONS = 'UserStartOptions';
+const SEEKING_MENTOR = "SeekingMentor";
+const CREATE_MENTOR_PROFILE = "CreateMentorProfile";
+const USER_START_OPTIONS = "UserStartOptions";
+
+export type EventOrValue =
+  | React.ChangeEvent<HTMLInputElement>
+  | string
+  | number;
+
+function isInputEvent(value: any): value is { target: HTMLInputElement } {
+  return value && isObject(value) && isObject(value.target);
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUser(request);
@@ -44,17 +39,19 @@ export const action: ActionFunction = async ({ request }) => {
   const requestText = await request.text();
   const form = new URLSearchParams(requestText);
   const userStartOptions: string[] = JSON.parse(
-    form.get(USER_START_OPTIONS) ?? '[]',
+    form.get(USER_START_OPTIONS) ?? "[]"
   );
   const isUserSeekingMentor = userStartOptions.includes(SEEKING_MENTOR);
   const isUserCreatingMentorProfile = userStartOptions.includes(
-    CREATE_MENTOR_PROFILE,
+    CREATE_MENTOR_PROFILE
   );
   if (isUserSeekingMentor && isUserCreatingMentorProfile) {
     return redirect(`${routes.startAbout}?create=true&seeking=true`);
-  } if (isUserSeekingMentor) {
+  }
+  if (isUserSeekingMentor) {
     return redirect(`${routes.startAbout}?create=false&seeking=true`);
-  } if (isUserCreatingMentorProfile) {
+  }
+  if (isUserCreatingMentorProfile) {
     return redirect(`${routes.startAbout}?create=true&seeking=false`);
   }
 };
@@ -63,110 +60,103 @@ function Start() {
   const data = useActionData();
   const transition = useTransition();
 
-  const { value, getCheckboxProps } = useCheckboxGroup({
-    defaultValue: [],
-  });
+  const [value, setValue] = React.useState<(string | number)[]>([]);
+
+  const handleChange = React.useCallback(
+    (eventOrValue: EventOrValue) => {
+      if (!value) return;
+
+      const isChecked = isInputEvent(eventOrValue)
+        ? eventOrValue.target.checked
+        : !value.includes(eventOrValue);
+
+      const selectedValue = isInputEvent(eventOrValue)
+        ? eventOrValue.target.value
+        : eventOrValue;
+
+      const nextValue = isChecked
+        ? [...value, selectedValue]
+        : value.filter((v) => String(v) !== String(selectedValue));
+
+      console.log(nextValue);
+
+      setValue(nextValue);
+    },
+    [setValue, value]
+  );
 
   return (
-    <Flex
-      minH="100vh"
-      align="center"
-      justify="center"
-      bg={useColorModeValue('gray.50', 'gray.800')}
-    >
-      <Stack spacing={8} mx="auto" maxW="lg" py={12} px={6}>
-        <Stack align="center">
-          <Image src={Logo} />
-          <Heading fontSize="4xl">How can we help?</Heading>
-          <Text fontSize="lg" color="gray.600">
+    <div className="flex min-h-screen content-center items-center justify-center">
+      <div className="flex  flex-col space-y-8 mx-auto max-w-lg py-12 px-6">
+        <div className="flex  flex-col content-center items-center">
+          <img className="mx-auto h-12  w-auto" src={Logo} alt="Hoots" />
+          <H4>How can we help?</H4>
+          <Paragraph textColorClassName="dark:text-gray-300 text-gray-600">
             – Select one or more options to get started –
-          </Text>
-        </Stack>
-        <Box
-          rounded="lg"
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow="lg"
-          p={8}
-        >
+          </Paragraph>
+        </div>
+        <div className="rounded-lg dark:bg-gray-700 shadow-lg p-8">
           <Form method="post">
-            <Text fontSize="lg" color="gray.600">
+            <Paragraph textColorClassName="dark:text-white">
               I want to...
-            </Text>
+            </Paragraph>
             <input
+              readOnly
               value={JSON.stringify(value)}
               hidden
               name={USER_START_OPTIONS}
             />
-            <Grid
-              templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
-              gap={5}
-              pt="5"
-            >
-              <GridItem>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pt-5">
+              <div>
                 <LargeCheckBox
-                  label="Find A Mentor"
+                  label="Find a mentor"
                   name={SEEKING_MENTOR}
-                  {...getCheckboxProps({ value: SEEKING_MENTOR })}
-                  icon={(
+                  onChange={handleChange}
+                  icon={
                     <FontAwesomeIcon
                       icon={faMagnifyingGlass}
                       size="2x"
                       color="#718096"
                     />
-                  )}
+                  }
                 />
-              </GridItem>
-              <GridItem>
+              </div>
+              <div>
                 <LargeCheckBox
                   label="Create a mentor profile"
                   name={CREATE_MENTOR_PROFILE}
-                  {...getCheckboxProps({ value: CREATE_MENTOR_PROFILE })}
-                  icon={(
+                  onChange={handleChange}
+                  icon={
                     <FontAwesomeIcon
                       icon={faPersonCirclePlus}
                       size="2x"
                       color="#718096"
                     />
-                  )}
+                  }
                 />
-              </GridItem>
-            </Grid>
-            <Stack
-              spacing={4}
-              direction="row"
-              align="center"
-              justifyContent="end"
-              pt="5"
-            >
-              <Text color="red.500">{data?.error}</Text>
+              </div>
+            </div>
+
+            <div className="flex space-x-4 flex-row items-center justify-end pt-5">
+              <Paragraph textColorClassName="text-red-500">
+                {data?.error}
+              </Paragraph>
               <Button
-                background="brand.500"
-                textColor="white"
-                isLoading={transition.state === 'submitting'}
                 type="submit"
-                _hover={{ backgroundColor: 'brand.200' }}
                 disabled={!value.length}
+                variant="primary"
+                loading={transition.state === "submitting"}
               >
                 Next
               </Button>
-              <Link
-                href={routes.home}
-                style={{ textDecoration: 'none', display: 'flex' }}
-                _focus={{ boxShadow: 'none' }}
-              >
-                <Button
-                  background="gray.500"
-                  textColor="white"
-                  _hover={{ backgroundColor: 'gray.300' }}
-                >
-                  Skip
-                </Button>
+              <Link to={routes.home}>
+                <Button>Skip</Button>
               </Link>
-            </Stack>
+            </div>
           </Form>
-        </Box>
-      </Stack>
-    </Flex>
+        </div>
+      </div>
+    </div>
   );
 }
 
